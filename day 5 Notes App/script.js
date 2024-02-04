b8ec8ccdfe87e80ae7d2ff0c8593d061eeac7786
Hademinne <hademinne@gmail.com>
2024-02-04 10:54:08 +0000

    adding functionality

--- day 5 Notes App/script.js
+++ day 5 Notes App/script.js
@@ -2,6 +2,17 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem(
+    "notes",
+    notesContainer.innerHTML
+  );
+}
+
+function updateStorages() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
@@ -9,4 +20,18 @@ createBtn.addEventListener("click", () => {
   inputBox.setAttribute("contenteditable", "true");
   img.src = "images/delete.png";
   notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorages();
+  } else if (e.target.tagName === "p") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorages();
+      };
+    });
+  }
 });
