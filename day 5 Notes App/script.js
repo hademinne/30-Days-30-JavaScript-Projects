8fb298891aa1ca4e43d67c1a4cebf82ee4d06700
Hademinne <hademinne@gmail.com>
2024-02-05 09:16:16 +0000

    updateStorages function

--- day 5 Notes App/script.js
+++ day 5 Notes App/script.js
@@ -3,13 +3,11 @@ const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
 function showNotes() {
-  notesContainer.innerHTML = localStorage.getItem(
-    "notes",
-    notesContainer.innerHTML
-  );
+  notesContainer.innerHTML = localStorage.getItem("notes");
 }
+showNotes();
 
-function updateStorages() {
+function updateStorage() {
   localStorage.setItem("notes", notesContainer.innerHTML);
 }
 
@@ -25,12 +23,12 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
-    updateStorages();
-  } else if (e.target.tagName === "p") {
+    updateStorage();
+  } else if (e.target.tagName === "P") {
     notes = document.querySelectorAll(".input-box");
     notes.forEach((nt) => {
       nt.onkeyup = function () {
-        updateStorages();
+        updateStorage();
       };
     });
   }
